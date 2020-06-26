e1a2fb5ff30c2abeb84c1e228178b423e9db96c4
Kevin Kuang <kevinkuang23@gmail.com>
2020-06-26 21:49:55 +1000

    Cleaned files

--- client.py
+++ client.py
@@ -1,8 +1,5 @@
 # USAGE
-# python client.py --server-ip 192.168.1.103
-    
-    
-
+# python client.py --server-ip SERVER_IP
 
 # import the necessary packages
 from imutils.video import VideoStream
@@ -10,6 +7,9 @@ import imagezmq
 import argparse
 import socket
 import time
+
+# can find this on windows using command prompt: ipconfig
+SERVER_IP = '192.168.1.103'
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -19,15 +19,18 @@ args = vars(ap.parse_args())
 
 print("initialising imagesender")
 # initialize the ImageSender object with the socket address of the server
+sender = imagezmq.ImageSender(connect_to=f"tcp://{SERVER_IP}:5555")
 # sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
 # 	args["server_ip"]))
 print("initalised, getting clienthostname")
+
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
 rpiName = socket.gethostname()
-# vs = VideoStream(usePiCamera=True).start()
 print(f"name = {rpiName}")
-vs = VideoStream(src=1).start()
+
+# vs = VideoStream(usePiCamera=True).start()
+vs = VideoStream(src=1).start()	# use webcam instead of picamera
 time.sleep(2.0)
  
 print("start loop")
