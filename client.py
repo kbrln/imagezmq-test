706a8288a8ed6febf2667534cbd80da86ffa9e8e
Nissan Silvia <200sxmeda@gmail.com>
2020-06-26 21:07:27 +1000

    First com

---
+++ client.py
@@ -0,0 +1,36 @@
+# USAGE
+# python client.py --server-ip SERVER_IP
+
+# import the necessary packages
+from imutils.video import VideoStream
+import imagezmq
+import argparse
+import socket
+import time
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--server-ip", required=True,
+	help="ip address of the server to which the client will connect")
+args = vars(ap.parse_args())
+
+print("initialising imagesender")
+# initialize the ImageSender object with the socket address of the
+# server
+sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+	args["server_ip"]))
+print("initalised, getting clienthostname")
+# get the host name, initialize the video stream, and allow the
+# camera sensor to warmup
+rpiName = socket.gethostname()
+# vs = VideoStream(usePiCamera=True).start()
+print(f"name = {rpiName}")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+ 
+print("start loop")
+while True:
+	# read the frame from the camera and send it to the server
+	frame = vs.read()
+	print("frame", frame)
+	sender.send_image(rpiName, frame)
